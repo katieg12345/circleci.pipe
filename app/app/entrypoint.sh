3c11caf56fc923968f81e4bac65bf04a8a9fef18
Administrator <admin@example.com>
2023-07-09 23:13:12 +0300

    circleci

--- app/app/entrypoint.sh
+++ app/app/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# if [[ -n "${KUBERNETES_SERVICE_HOST}" ]]; then
-# # Source the secrets file, this will export the API_KEY environment variable
-# source /vault/secrets/apiKey
-# fi
+if [[ -n "${KUBERNETES_SERVICE_HOST}" ]]; then
+  while [ ! -f /vault/secrets/apikey ];do
+    sleep 1
+  done
+
+# Source the secrets file, this will export the API_KEY environment variable
+source /vault/secrets/apiKey
+fi
+
 # Execute the gunicorn command
 exec gunicorn --bind=0.0.0.0:${BIND_PORT} --workers=${WORKERS} --log-level=${LOG_LEVEL} project:app
