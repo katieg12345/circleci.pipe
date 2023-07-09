d2d85d33fd1ecc98627bc08d909a2cfe7d895a2f
Administrator <admin@example.com>
2023-07-09 22:21:28 +0300

    circleci

--- app/app/entrypoint.sh
+++ app/app/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Source the secrets file, this will export the API_KEY environment variable
-source /vault/secrets/apiKey
+# source /vault/secrets/apiKey
 
 # Execute the gunicorn command
 exec gunicorn --bind=0.0.0.0:${BIND_PORT} --workers=${WORKERS} --log-level=${LOG_LEVEL} project:app
